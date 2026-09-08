6ce77963ceff5da7ae2235b0adfea80a91c2afef
SHASHI PATHARIYA <pathariyashashi@gmail.com>
2026-09-08 12:19:42 +0530

    Update database.py

--- rural-doctor-backend/app/database.py
+++ rural-doctor-backend/app/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-from app.config import settings
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -14,10 +17,8 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
